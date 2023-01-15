20c7cb7f7ee51c0aa2dd0b54a6d06e726b359f54
barkwoofdog <97663379+barkwoofdog@users.noreply.github.com>
2023-01-15 00:08:02 -0600

    version 1.0
    ready for prod!

--- wireguardISPfiles/wgconfigurator.sh
+++ wireguardISPfiles/wgconfigurator.sh
@@ -3,14 +3,16 @@
 #wireguard setup utility
 
 
-#set file permissions. create file and add Private Key value to the file.
-(umask 077 && printf "[Interface] \n PrivateKey = " | sudo tee /etc/wireguard/wg1.conf) > /dev/null)
+#set file permissions. create file and add Private Key defintion to the file.
+(umask 077 && printf "[Interface]\nPrivateKey = " | sudo tee /etc/wireguard/wg1.conf > /dev/null)
 
-echo The following line is your Public Key\n
+echo The following line is your Public Key
 
-#generate public and private key. Append private to last line in the config file.
+#generate public and private key. Append private to the file so that it fills the definition previously created
 wg genkey | sudo tee -a /etc/wireguard/wg1.conf | wg pubkey | sudo tee /etc/wireguard/pubkey
 
+
+#begin to define the host. If it is Listener/Forwarder then define listen port.
 echo "Is this Host the VPS/Listening server? (y/n)"
 read listenAnswer
 
@@ -23,7 +25,7 @@ if [ $listenAnswer = "y" ]; then
         (printf "ListenPort = ") | tee -a /etc/wireguard/wg1.conf > /dev/null
         echo $userListen >>/etc/wireguard/wg1.conf
 
-        echo Listen Port $listenAnswer added.
+        echo Listen Port $userListen added.
 
 fi
 
@@ -42,13 +44,19 @@ echo "$addr" >> /etc/wireguard/wg1.conf
 #user will need to add their forward host into here
 if [ $listenAnswer = "y" ]; then
 
-     echo "" >> /etc/wireguard/wg1.conf   
+     echo "" >> /etc/wireguard/wg1.conf
 
      echo "PostUp = iptables -t nat -A PREROUTING -p tcp -i eth0 --match multiport '!' --dport 22 -j DNAT --to-destination HOMESERVER-WGIP; iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source THISHOST-IP
 PostUp = iptables -t nat -A PREROUTING -p udp -i eth0 '!' --dport $userListen -j DNAT --to-destination HOMESERVER-WGIP;
-
 PostDown = iptables -t nat -D PREROUTING -p tcp -i eth0 --match multiport '!' --dport 22 -j DNAT --to-destination HOMESERVER-WGIP; iptables -t nat -D POSTROUTING -o eth0 -j SNAT --to-source THISHOST-IP
 PostDown = iptables -t nat -D PREROUTING -p udp -i eth0 '!' --dport $userListen -j DNAT --to-destination HOMESERVER-WGIP;" >> /etc/wireguard/wg1.conf
+
+echo "" >> /etc/wireguard/wg1.conf
+
+#adds peer definition
+echo "[Peer]
+PublicKey = 
+AllowedIPs = /32" >> /etc/wireguard/wg1.conf
 
 printf "\n"
 
@@ -59,13 +67,16 @@ echo "ALSO, be wary of your interface. This script has assumed that your public 
 printf "\n"
 
 fi
- 
-#adds a peer definition for the other server.
 
-echo "[Peer]
+#adds a peer definition for the home server, which is a little different from the Listener
+if [ $listenAnswer = "n" ]; then
+        echo "" >> /etc/wireguard/wg1.conf
+        echo "[Peer]
 PublicKey = 
-AllowedIPs = /32" >> /etc/wireguard/wg1.conf
+AllowedIPs = 0.0.0.0/1, 128.0.0.0/1
+Endpoint = 
+PersistentKeepalive = 15" >> /etc/wireguard/wg1.conf
+fi
 
 
 echo "Dog's Wireguard Configurator has finished!"
-
